826baef0d65460721fd4edc939741b895449e63e
Bader <bander99u@gmail.com>
2026-05-03 23:08:43 +0300

    initializing Vue

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+    export default{
+
+    }
+</script>
+<template>
+  <h1>Vue Jobs</h1>
+  <p v-if="status">User is active! </p>
+  <p v-else>User is not active!</p>
+  <button  @click="toggleStatus"> change status</button>
+  <li>
+    <ul v-for="task in tasks" key="task">{{ task }}</ul>
+  </li>
+  <a :href="link">Click for google</a>
+</template>
+
---
+++ src/App2.vue
@@ -0,0 +1,32 @@
+<script>
+    export default{
+      data() {
+        return {
+          name: 'Ali selam',
+          status:false,
+          tasks:['cleaning','mopping','reading'],
+          link:"https://google.com"
+        }
+      },
+      methods:{
+        toggleStatus(){
+          if (this.status===true){
+            this.status=false
+          }else{
+            this.status=true
+          }
+        }
+      }
+    }
+</script>
+<template>
+  <h1>Vue Jobs</h1>
+  <p v-if="status">User is active! </p>
+  <p v-else>User is not active!</p>
+  <button  @click="toggleStatus"> change status</button>
+  <li>
+    <ul v-for="task in tasks" key="task">{{ task }}</ul>
+  </li>
+  <a :href="link">Click for google</a>
+</template>
+
