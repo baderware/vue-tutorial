fda54ca90d29ecf9dfe0924dfa18ab36135daedb
Bader <bander99u@gmail.com>
2026-05-05 15:10:14 +0300

    finished Home and NotFoundView code

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Hero from '@/components/Hero.vue';
+import HomeCards from '@/components/HomeCards.vue';
+import JobListing from '@/components/JobListing.vue';
+</script>
+
+<template>
+  <Hero title="Test-title"/>
+  <HomeCards />
+  <JobListing :limit="2" :show-button="true" />
+</template>
